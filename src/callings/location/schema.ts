119fd3084921c5e0266058499f34fbbc068665a9
Bookman0001 <odonel51486666@gmail.com>
2025-07-21 19:56:23 +0900

    fix: prompt

---
+++ src/callings/location/schema.ts
@@ -0,0 +1,10 @@
+import z from 'zod'
+
+export const validationSchema = z.object({
+  latitude: z
+    .number()
+    .describe('It should be addressed based on the location from user input.'),
+  longitude: z
+    .number()
+    .describe('It should be addressed based on the location from user input.'),
+})
